6397ab49dbb9017d0f28a2f0fe791f54deeba846
Maxim Trüber <63929717+Max-Trueby@users.noreply.github.com>
2023-12-20 21:08:42 +0100

    del all

---
+++ offline/src/types/social.ts
@@ -0,0 +1,12 @@
+export type Social = {
+  url: string;
+  icon: any;
+  name: string;
+}
+
+export type TechStack = {
+  color: string;
+  name: string;
+  icon: any;
+  url: string;
+}
--- src/types/gitRepo.ts
+++
@@ -1,9 +0,0 @@
-export type GitRepo = {
-  name: string,
-  description: string,
-  language: string,
-  url: string,
-  stargazers_count: number,
-  forks_count: number,
-  clone_url: string,
-}
